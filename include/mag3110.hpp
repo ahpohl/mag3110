0c648db0c01dea7521832b8c744b7e3d0eca2de4
Alexander Pohl <alex@ahpohl.com>
2019-10-11 18:42:04 +0200

    static constants no compile error

--- include/mag3110.hpp
+++ include/mag3110.hpp
@@ -1,94 +1,94 @@
 #ifndef MAG3110_HPP
 #define MAG3110_HPP
 #include <cstdint>
-#include <cmath>
 
 class MAG3110
 {
 public:
-  static uint8_t const MAG3110_I2C_ADDRESS = 0x0E;
-  static uint8_t const MAG3110_WHO_AM_I_RSP = 0xC4;
-  static int const CALIBRATION_TIMEOUT = 10000; // ms
-  static double constexpr DEG_PER_RAD = (180.0 / 3.14159265358979);
+  static uint8_t const MAG3110_I2C_ADDRESS;
+  static uint8_t const MAG3110_WHO_AM_I_RSP;
+  static int const CALIBRATION_TIMEOUT;
+  static double const DEG_PER_RAD;
   
   // register addresses
-  static uint8_t const MAG3110_DR_STATUS = 0x00;
-  static uint8_t const MAG3110_OUT_X_MSB = 0x01;
-  static uint8_t const MAG3110_OUT_X_LSB = 0x02;
-  static uint8_t const MAG3110_OUT_Y_MSB = 0x03;
-  static uint8_t const MAG3110_OUT_Y_LSB = 0x04;
-  static uint8_t const MAG3110_OUT_Z_MSB = 0x05;
-  static uint8_t const MAG3110_OUT_Z_LSB = 0x06;
-  static uint8_t const MAG3110_WHO_AM_I =	 0x07;
-  static uint8_t const MAG3110_SYSMOD =		 0x08;
-  static uint8_t const MAG3110_OFF_X_MSB = 0x09;
-  static uint8_t const MAG3110_OFF_X_LSB = 0x0A;
-  static uint8_t const MAG3110_OFF_Y_MSB = 0x0B;
-  static uint8_t const MAG3110_OFF_Y_LSB = 0x0C;
-  static uint8_t const MAG3110_OFF_Z_MSB = 0x0D;
-  static uint8_t const MAG3110_OFF_Z_LSB = 0x0E;
-  static uint8_t const MAG3110_DIE_TEMP =  0x0F;
-  static uint8_t const MAG3110_CTRL_REG1 = 0x10;
-  static uint8_t const MAG3110_CTRL_REG2 = 0x11;
+  static uint8_t const MAG3110_DR_STATUS;
+  static uint8_t const MAG3110_OUT_X_MSB;
+  static uint8_t const MAG3110_OUT_X_LSB;
+  static uint8_t const MAG3110_OUT_Y_MSB;
+  static uint8_t const MAG3110_OUT_Y_LSB;
+  static uint8_t const MAG3110_OUT_Z_MSB;
+  static uint8_t const MAG3110_OUT_Z_LSB;
+  static uint8_t const MAG3110_WHO_AM_I;
+  static uint8_t const MAG3110_SYSMOD;
+  static uint8_t const MAG3110_OFF_X_MSB;
+  static uint8_t const MAG3110_OFF_X_LSB;
+  static uint8_t const MAG3110_OFF_Y_MSB;
+  static uint8_t const MAG3110_OFF_Y_LSB;
+  static uint8_t const MAG3110_OFF_Z_MSB;
+  static uint8_t const MAG3110_OFF_Z_LSB;
+  static uint8_t const MAG3110_DIE_TEMP;
+  static uint8_t const MAG3110_CTRL_REG1;
+  static uint8_t const MAG3110_CTRL_REG2;
   
   // CTRL_REG1 Settings
   // Output Data Rate/Oversample Settings
   // DR_OS_80_16 -> Output Data Rate = 80Hz, 
   // Oversampling Ratio = 16
-  static uint8_t const MAG3110_DR_OS_80_16 =    0x00;
-  static uint8_t const MAG3110_DR_OS_40_32 =		0x08;
-  static uint8_t const MAG3110_DR_OS_20_64 =		0x10;
-  static uint8_t const MAG3110_DR_OS_10_128 =	  0x18;
-  static uint8_t const MAG3110_DR_OS_40_16 =		0x20;
-  static uint8_t const MAG3110_DR_OS_20_32 =		0x28;
-  static uint8_t const MAG3110_DR_OS_10_64 =		0x30;
-  static uint8_t const MAG3110_DR_OS_5_128 =		0x38;
-  static uint8_t const MAG3110_DR_OS_20_16 =		0x40;
-  static uint8_t const MAG3110_DR_OS_10_32 =		0x48;
-  static uint8_t const MAG3110_DR_OS_5_64 =		  0x50;
-  static uint8_t const MAG3110_DR_OS_2_5_128 =	0x58;
-  static uint8_t const MAG3110_DR_OS_10_16 =		0x60;
-  static uint8_t const MAG3110_DR_OS_5_32 =		  0x68;
-  static uint8_t const MAG3110_DR_OS_2_5_64 =   0x70;
-  static uint8_t const MAG3110_DR_OS_1_25_128 = 0x78;
-  static uint8_t const MAG3110_DR_OS_5_16 =     0x80;
-  static uint8_t const MAG3110_DR_OS_2_5_32 =	  0x88;
-  static uint8_t const MAG3110_DR_OS_1_25_64	= 0x90;
-  static uint8_t const MAG3110_DR_OS_0_63_128 = 0x98;
-  static uint8_t const MAG3110_DR_OS_2_5_16 =   0xA0;
-  static uint8_t const MAG3110_DR_OS_1_25_32 =  0xA8;
-  static uint8_t const MAG3110_DR_OS_0_63_64 =  0xB0;
-  static uint8_t const MAG3110_DR_OS_0_31_128 = 0xB8;
-  static uint8_t const MAG3110_DR_OS_1_25_16 =  0xC0;
-  static uint8_t const MAG3110_DR_OS_0_63_32 =  0xC8;
-  static uint8_t const MAG3110_DR_OS_0_31_64 =  0xD0;
-  static uint8_t const MAG3110_DR_OS_0_16_128 = 0xD8;
-  static uint8_t const MAG3110_DR_OS_0_63_16 =  0xE0;
-  static uint8_t const MAG3110_DR_OS_0_31_32 =  0xE8;
-  static uint8_t const MAG3110_DR_OS_0_16_64 =  0xF0;
-  static uint8_t const MAG3110_DR_OS_0_08_128 = 0xF8;
+  static uint8_t const MAG3110_DR_OS_80_16;
+  static uint8_t const MAG3110_DR_OS_40_32;
+  static uint8_t const MAG3110_DR_OS_20_64;
+  static uint8_t const MAG3110_DR_OS_10_128;
+  static uint8_t const MAG3110_DR_OS_40_16;
+  static uint8_t const MAG3110_DR_OS_20_32;
+  static uint8_t const MAG3110_DR_OS_10_64;
+  static uint8_t const MAG3110_DR_OS_5_128;
+  static uint8_t const MAG3110_DR_OS_20_16;
+  static uint8_t const MAG3110_DR_OS_10_32;
+  static uint8_t const MAG3110_DR_OS_5_64;
+  static uint8_t const MAG3110_DR_OS_2_5_128;
+  static uint8_t const MAG3110_DR_OS_10_16;
+  static uint8_t const MAG3110_DR_OS_5_32;
+  static uint8_t const MAG3110_DR_OS_2_5_64;
+  static uint8_t const MAG3110_DR_OS_1_25_128;
+  static uint8_t const MAG3110_DR_OS_5_16;
+  static uint8_t const MAG3110_DR_OS_2_5_32;
+  static uint8_t const MAG3110_DR_OS_1_25_64;
+  static uint8_t const MAG3110_DR_OS_0_63_128;
+  static uint8_t const MAG3110_DR_OS_2_5_16;
+  static uint8_t const MAG3110_DR_OS_1_25_32;
+  static uint8_t const MAG3110_DR_OS_0_63_64;
+  static uint8_t const MAG3110_DR_OS_0_31_128;
+  static uint8_t const MAG3110_DR_OS_1_25_16;
+  static uint8_t const MAG3110_DR_OS_0_63_32;
+  static uint8_t const MAG3110_DR_OS_0_31_64;
+  static uint8_t const MAG3110_DR_OS_0_16_128;
+  static uint8_t const MAG3110_DR_OS_0_63_16;
+  static uint8_t const MAG3110_DR_OS_0_31_32;
+  static uint8_t const MAG3110_DR_OS_0_16_64;
+  static uint8_t const MAG3110_DR_OS_0_08_128;
 
   // Other CTRL_REG1 Settings
-  static uint8_t const MAG3110_FAST_READ = 			     0x04;
-  static uint8_t const MAG3110_TRIGGER_MEASUREMENT = 0x02;
-  static uint8_t const MAG3110_ACTIVE_MODE =			   0x01;
-  static uint8_t const MAG3110_STANDBY_MODE =		     0x00;
+  static uint8_t const MAG3110_FAST_READ;
+  static uint8_t const MAG3110_TRIGGER_MEASUREMENT;
+  static uint8_t const MAG3110_ACTIVE_MODE;
+  static uint8_t const MAG3110_STANDBY_MODE;
 
   // CTRL_REG2 Settings
-  static uint8_t const MAG3110_AUTO_MRST_EN = 0x80;
-  static uint8_t const MAG3110_RAW_MODE = 	  0x20;
-  static uint8_t const MAG3110_NORMAL_MODE =	0x00;
-  static uint8_t const MAG3110_MAG_RST =		  0x10;
+  static uint8_t const MAG3110_AUTO_MRST_EN;
+  static uint8_t const MAG3110_RAW_MODE;
+  static uint8_t const MAG3110_NORMAL_MODE;
+  static uint8_t const MAG3110_MAG_RST;
 
   // SYSMOD Readings
-  static uint8_t const MAG3110_SYSMOD_STANDBY =    0x00;
-  static uint8_t const MAG3110_SYSMOD_ACTIVE_RAW = 0x01;
-  static uint8_t const	MAG3110_SYSMOD_ACTIVE	=	   0x02;
-  static uint8_t const MAG3110_X_AXIS =            0x01;
-  static uint8_t const MAG3110_Y_AXIS =            0x03;
-  static uint8_t const MAG3110_Z_AXIS =            0x05;
+  static uint8_t const MAG3110_SYSMOD_STANDBY;
+  static uint8_t const MAG3110_SYSMOD_ACTIVE_RAW;
+  static uint8_t const MAG3110_SYSMOD_ACTIVE;
 
-  // methods
+  // axis base address
+  static uint8_t const MAG3110_X_AXIS;
+  static uint8_t const MAG3110_Y_AXIS;
+  static uint8_t const MAG3110_Z_AXIS;
+
   MAG3110(void);
   ~MAG3110(void);
   void setDebug(void);
@@ -127,7 +127,6 @@ private:
   bool m_activeMode;
   bool m_rawMode;
   bool m_calibrated;
-  bool m_error;
   int m_fd;
   int m_xmin;
   int m_xmax;
--- src/mag3110.cpp
+++ src/mag3110.cpp
@@ -14,6 +14,88 @@
 
 using namespace std;
 
+uint8_t const MAG3110::MAG3110_I2C_ADDRESS = 0x0E;
+uint8_t const MAG3110::MAG3110_WHO_AM_I_RSP = 0xC4;
+int const MAG3110::CALIBRATION_TIMEOUT = 10000; // ms
+double const MAG3110::DEG_PER_RAD = (180.0 / M_PI);
+  
+// register addresses
+uint8_t const MAG3110::MAG3110_DR_STATUS = 0x00;
+uint8_t const MAG3110::MAG3110_OUT_X_MSB = 0x01;
+uint8_t const MAG3110::MAG3110_OUT_X_LSB = 0x02;
+uint8_t const MAG3110::MAG3110_OUT_Y_MSB = 0x03;
+uint8_t const MAG3110::MAG3110_OUT_Y_LSB = 0x04;
+uint8_t const MAG3110::MAG3110_OUT_Z_MSB = 0x05;
+uint8_t const MAG3110::MAG3110_OUT_Z_LSB = 0x06;
+uint8_t const MAG3110::MAG3110_WHO_AM_I =	 0x07;
+uint8_t const MAG3110::MAG3110_SYSMOD =		 0x08;
+uint8_t const MAG3110::MAG3110_OFF_X_MSB = 0x09;
+uint8_t const MAG3110::MAG3110_OFF_X_LSB = 0x0A;
+uint8_t const MAG3110::MAG3110_OFF_Y_MSB = 0x0B;
+uint8_t const MAG3110::MAG3110_OFF_Y_LSB = 0x0C;
+uint8_t const MAG3110::MAG3110_OFF_Z_MSB = 0x0D;
+uint8_t const MAG3110::MAG3110_OFF_Z_LSB = 0x0E;
+uint8_t const MAG3110::MAG3110_DIE_TEMP =  0x0F;
+uint8_t const MAG3110::MAG3110_CTRL_REG1 = 0x10;
+uint8_t const MAG3110::MAG3110_CTRL_REG2 = 0x11;
+  
+// CTRL_REG1 Settings
+// Output Data Rate/Oversample Settings
+// DR_OS_80_16 -> Output Data Rate = 80Hz, 
+// Oversampling Ratio = 16
+uint8_t const MAG3110::MAG3110_DR_OS_80_16 =    0x00;
+uint8_t const MAG3110::MAG3110_DR_OS_40_32 =		0x08;
+uint8_t const MAG3110::MAG3110_DR_OS_20_64 =		0x10;
+uint8_t const MAG3110::MAG3110_DR_OS_10_128 =	  0x18;
+uint8_t const MAG3110::MAG3110_DR_OS_40_16 =		0x20;
+uint8_t const MAG3110::MAG3110_DR_OS_20_32 =		0x28;
+uint8_t const MAG3110::MAG3110_DR_OS_10_64 =		0x30;
+uint8_t const MAG3110::MAG3110_DR_OS_5_128 =		0x38;
+uint8_t const MAG3110::MAG3110_DR_OS_20_16 =		0x40;
+uint8_t const MAG3110::MAG3110_DR_OS_10_32 =		0x48;
+uint8_t const MAG3110::MAG3110_DR_OS_5_64 =		  0x50;
+uint8_t const MAG3110::MAG3110_DR_OS_2_5_128 =	0x58;
+uint8_t const MAG3110::MAG3110_DR_OS_10_16 =		0x60;
+uint8_t const MAG3110::MAG3110_DR_OS_5_32 =		  0x68;
+uint8_t const MAG3110::MAG3110_DR_OS_2_5_64 =   0x70;
+uint8_t const MAG3110::MAG3110_DR_OS_1_25_128 = 0x78;
+uint8_t const MAG3110::MAG3110_DR_OS_5_16 =     0x80;
+uint8_t const MAG3110::MAG3110_DR_OS_2_5_32 =	  0x88;
+uint8_t const MAG3110::MAG3110_DR_OS_1_25_64	= 0x90;
+uint8_t const MAG3110::MAG3110_DR_OS_0_63_128 = 0x98;
+uint8_t const MAG3110::MAG3110_DR_OS_2_5_16 =   0xA0;
+uint8_t const MAG3110::MAG3110_DR_OS_1_25_32 =  0xA8;
+uint8_t const MAG3110::MAG3110_DR_OS_0_63_64 =  0xB0;
+uint8_t const MAG3110::MAG3110_DR_OS_0_31_128 = 0xB8;
+uint8_t const MAG3110::MAG3110_DR_OS_1_25_16 =  0xC0;
+uint8_t const MAG3110::MAG3110_DR_OS_0_63_32 =  0xC8;
+uint8_t const MAG3110::MAG3110_DR_OS_0_31_64 =  0xD0;
+uint8_t const MAG3110::MAG3110_DR_OS_0_16_128 = 0xD8;
+uint8_t const MAG3110::MAG3110_DR_OS_0_63_16 =  0xE0;
+uint8_t const MAG3110::MAG3110_DR_OS_0_31_32 =  0xE8;
+uint8_t const MAG3110::MAG3110_DR_OS_0_16_64 =  0xF0;
+uint8_t const MAG3110::MAG3110_DR_OS_0_08_128 = 0xF8;
+
+// Other CTRL_REG1 Settings
+uint8_t const MAG3110::MAG3110_FAST_READ = 			     0x04;
+uint8_t const MAG3110::MAG3110_TRIGGER_MEASUREMENT = 0x02;
+uint8_t const MAG3110::MAG3110_ACTIVE_MODE =			   0x01;
+uint8_t const MAG3110::MAG3110_STANDBY_MODE =		     0x00;
+
+// CTRL_REG2 Settings
+uint8_t const MAG3110::MAG3110_AUTO_MRST_EN = 0x80;
+uint8_t const MAG3110::MAG3110_RAW_MODE = 	  0x20;
+uint8_t const MAG3110::MAG3110_NORMAL_MODE =	0x00;
+uint8_t const MAG3110::MAG3110_MAG_RST =		  0x10;
+
+// SYSMOD Readings
+uint8_t const MAG3110::MAG3110_SYSMOD_STANDBY =    0x00;
+uint8_t const MAG3110::MAG3110_SYSMOD_ACTIVE_RAW = 0x01;
+uint8_t const MAG3110::MAG3110_SYSMOD_ACTIVE	=	   0x02;
+uint8_t const MAG3110::MAG3110_X_AXIS =            0x01;
+uint8_t const MAG3110::MAG3110_Y_AXIS =            0x03;
+uint8_t const MAG3110::MAG3110_Z_AXIS =            0x05;
+
 MAG3110::MAG3110(void)
 {
   m_debug = false;
