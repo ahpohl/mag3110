a29fe58321a86a17fd86d9115f2644a11938d347
Alexander Pohl <alex@ahpohl.com>
2019-10-22 23:01:49 +0200

    documention the functions, wip

--- include/mag3110.hpp
+++ include/mag3110.hpp
@@ -2,15 +2,14 @@
 #define MAG3110_HPP
 #include <cstdint>
 
-/**
- * \brief A brief history of JavaDoc-style (C-style) comments.
- *
- * This is the typical JavaDoc-style C-style comment. It starts with two
- * asterisks.
- *
- * @param theory Even if there is only one possible unified theory. it is just a
- *               set of rules and equations.
- */
+/** @brief A class to drive the mag3110 3-axis magnetometer
+
+    This class is derived from the Arduino library created by Sparkfun and
+    has been ported from the Arduino to plain C++.
+ 
+    @author Alexander Pohl <webmaster@ahpohl.com>
+    @date October 2019
+    */
 
 class MAG3110
 {
@@ -19,8 +18,12 @@ public:
   static uint8_t const MAG3110_WHO_AM_I_RSP;
   static int const CALIBRATION_TIMEOUT;
   static int const MAG3110_DIE_TEMP_OFFSET;
-  
-  // register addresses
+ 
+  /** @name Register addresses
+      
+      These attributes describe the register addresses
+      */
+  ///@{
   static uint8_t const MAG3110_DR_STATUS;
   static uint8_t const MAG3110_OUT_X_MSB;
   static uint8_t const MAG3110_OUT_X_LSB;
@@ -39,11 +42,15 @@ public:
   static uint8_t const MAG3110_DIE_TEMP;
   static uint8_t const MAG3110_CTRL_REG1;
   static uint8_t const MAG3110_CTRL_REG2;
+  ///@}
   
-  // CTRL_REG1 Settings
-  // Output Data Rate/Oversample Settings
-  // DR_OS_80_16 -> Output Data Rate = 80Hz, 
-  // Oversampling Ratio = 16
+  /** @name CTRL_REG1 settings: data rate and oversampling ratio
+      
+      These attributes select the data rate and oversampling ratio.
+      For example DR_OS_80_16 -> Output Data Rate = 80Hz, 
+      oversampling ratio = 16.
+      */
+  ///@{
   static uint8_t const MAG3110_DR_OS_80_16;
   static uint8_t const MAG3110_DR_OS_40_32;
   static uint8_t const MAG3110_DR_OS_20_64;
@@ -76,26 +83,34 @@ public:
   static uint8_t const MAG3110_DR_OS_0_31_32;
   static uint8_t const MAG3110_DR_OS_0_16_64;
   static uint8_t const MAG3110_DR_OS_0_08_128;
+  ///@}
 
-  // Other CTRL_REG1 Settings
+  /** @name Other CTRL_REG1 settings
+      
+      These attributes select a fast read mode, trigger a measurement and
+      put the chip into active mode
+      */
+  ///@{
   static uint8_t const MAG3110_FAST_READ;
   static uint8_t const MAG3110_TRIGGER_MEASUREMENT;
   static uint8_t const MAG3110_ACTIVE_MODE;
+  ///@}
 
-  // CTRL_REG2 Settings
+  /** @name CTRL_REG2 settings
+      
+      These attributes enable an auto magnetic reset, put the chip into 
+      raw mode ignoring the offset correction, and perform a magnetic reset
+      */
+  ///@{
   static uint8_t const MAG3110_AUTO_MRST_EN;
   static uint8_t const MAG3110_RAW_MODE;
   static uint8_t const MAG3110_MAG_RST;
+  ///@}
 
   // SYSMOD Readings
   static uint8_t const MAG3110_SYSMOD_STANDBY;
   static uint8_t const MAG3110_SYSMOD_ACTIVE_RAW;
   static uint8_t const MAG3110_SYSMOD_ACTIVE;
-
-  // axis base address
-  static uint8_t const MAG3110_X_AXIS;
-  static uint8_t const MAG3110_Y_AXIS;
-  static uint8_t const MAG3110_Z_AXIS;
 
   // DR_STATUS readings
   static uint8_t const MAG3110_DR_STATUS_XDR;
@@ -107,9 +122,33 @@ public:
   static uint8_t const MAG3110_DR_STATUS_ZOW;
   static uint8_t const MAG3110_DR_STATUS_XYZOW;
 
+  // axis base address
+  static uint8_t const MAG3110_X_AXIS;
+  static uint8_t const MAG3110_Y_AXIS;
+  static uint8_t const MAG3110_Z_AXIS;
+
+  /** @brief Default constructor
+      
+      The default constructor initializes the private member variables.
+      */
   MAG3110(void);
+  /** @brief Default descructor
+      
+      The default destructor removes the I<sup>2</sup>C bus handle
+      */
   ~MAG3110(void);
+  /** @brief Control debug output
+      
+      This function enables or disables debug output of the member functions.  
+      @param t_debug boolean flag
+      */
   void setDebug(bool t_debug);
+  /** @brief Initializes I<sup>2</sup>C communication
+      
+      This funtion initializes the I<sup>2</sup>C communication with the chip
+      and identifies the chip as a MAG3110.
+      @param t_bus The I<sup>2</sup>C bus device, i.e.\ /dev/i2c-1
+      */
   void initialize(const char* t_bus);
   uint8_t readRegister(uint8_t const& t_addr) const;
   void writeRegister(uint8_t const& t_addr, uint8_t const& t_val) const;
