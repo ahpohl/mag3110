504a4deec24c4b0243c7dd78bb4a4cf0a79ee1c5
Alexander Pohl <alex@ahpohl.com>
2019-10-23 20:14:39 +0200

    continue doc member functions

--- include/mag3110.hpp
+++ include/mag3110.hpp
@@ -4,8 +4,8 @@
 
 /** @brief A class to drive the mag3110 3-axis magnetometer
 
-    This class is derived from the Arduino library created by Sparkfun and
-    has been ported from the Arduino to plain C++.
+    This class is derived from the Sparkfun Arduino library and
+    has been ported to plain C++.
  
     @author Alexander Pohl <webmaster@ahpohl.com>
     @date October 2019
@@ -107,12 +107,22 @@ public:
   static uint8_t const MAG3110_MAG_RST;
   ///@}
 
-  // SYSMOD Readings
+  /** @name SYSMOD readings
+      
+      These attributes indicate the current device operation mode
+      */
+  ///@{
   static uint8_t const MAG3110_SYSMOD_STANDBY;
   static uint8_t const MAG3110_SYSMOD_ACTIVE_RAW;
   static uint8_t const MAG3110_SYSMOD_ACTIVE;
+  ///@}
 
-  // DR_STATUS readings
+  /** @name DR_STATUS readings
+
+      These attributes provide the aquisition status information of the OUT_X, 
+      OUT_Y, and OUT_Z registers
+      */
+  ///@{
   static uint8_t const MAG3110_DR_STATUS_XDR;
   static uint8_t const MAG3110_DR_STATUS_YDR;
   static uint8_t const MAG3110_DR_STATUS_ZDR;
@@ -121,18 +131,24 @@ public:
   static uint8_t const MAG3110_DR_STATUS_YOW;
   static uint8_t const MAG3110_DR_STATUS_ZOW;
   static uint8_t const MAG3110_DR_STATUS_XYZOW;
+  ///@}
 
-  // axis base address
+  /** @name axes base addresses
+
+      These attributes describe the base addresses of the magnetometer axes
+      */
+  ///@{
   static uint8_t const MAG3110_X_AXIS;
   static uint8_t const MAG3110_Y_AXIS;
   static uint8_t const MAG3110_Z_AXIS;
+  ///@}
 
   /** @brief Default constructor
       
       The default constructor initializes the private member variables.
       */
   MAG3110(void);
-  /** @brief Default descructor
+  /** @brief Default destcructor
       
       The default destructor removes the I<sup>2</sup>C bus handle
       */
@@ -140,23 +156,74 @@ public:
   /** @brief Control debug output
       
       This function enables or disables debug output of the member functions.  
-      @param t_debug boolean flag
+      
+      @param t_debug true: debug output enabled, false: debug output disabled
       */
   void setDebug(bool t_debug);
-  /** @brief Initializes I<sup>2</sup>C communication
+  /** @brief Initialize I<sup>2</sup>C communication
       
       This funtion initializes the I<sup>2</sup>C communication with the chip
       and identifies the chip as a MAG3110.
+      
       @param t_bus The I<sup>2</sup>C bus device, i.e.\ /dev/i2c-1
       */
   void initialize(const char* t_bus);
+  /** @brief Read a register
+
+      This function reads a register at the given address
+    
+      @param t_addr address of the register
+      */
   uint8_t readRegister(uint8_t const& t_addr) const;
+  /** @brief Write a register
+
+      This function writes a value to the register at the given address.
+      
+      @param t_addr address of the register
+      @param t_val value to write
+      */
   void writeRegister(uint8_t const& t_addr, uint8_t const& t_val) const;
+  /** @brief Standby
+
+      This function puts the chip into standby mode.
+      */
   void standby(void) const;
+  /** @brief Start
+
+      This function puts the chip into operation mode.
+      */
   void start(void) const;
+  /** @brief Reset
+
+      This function resets the control registers, selects the default
+      data rate and oversampling setting and resets the user provided
+      axes offsets.
+      */
   void reset(void);
+  /** @brief Test if active
+
+      This function tests whether the chip is in operation mode or in standby
+      mode.
+  
+      @returns true: chip is active, false: chip is in standby
+      */ 
   bool isActive(void) const;
+  /** @brief Test if raw mode
+
+      This function tests whether the chip is in active raw mode.
+  
+      @returns true: chip is in active raw mode, false: chip is not in active
+               raw mode
+      */
   bool isRaw(void) const;
+    /** @brief Set raw mode
+
+      This function puts the chip into active raw mode. In raw active mode,
+      the chip does not automatically apply the user offset correction and 
+      outputs the raw magnetometer reading. Useful for calibration.
+  
+      @returns true: chip is in active raw mode
+      */
   void setRawMode(bool const t_raw) const;
   void triggerMeasurement(void) const;
   bool dataReady(void) const;  
